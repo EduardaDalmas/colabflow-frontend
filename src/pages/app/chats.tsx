d84513b409311b518d1a1e03d89ccdbd82318daa
Eduarda <eduarda.dalmas@dcgroupsaude.com.br>
2024-08-15 16:22:19 -0300

    adicionados elementos visuais planejados no figma

--- src/pages/app/chats.tsx
+++ src/pages/app/chats.tsx
@@ -1,9 +1,18 @@
 import { Input } from '@/components/ui/input';
 import { useAuth } from '@/context/authProvider';
 import { Avatar, AvatarFallback } from '@radix-ui/react-avatar';
-import { ListCollapse, SendHorizonal } from 'lucide-react';
+import { Archive, CirclePlus, Link2, ListCollapse, SendHorizonal, Settings, UserPlus2 } from 'lucide-react';
 import { useEffect, useRef, useState } from 'react';
 import io from 'socket.io-client';
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+  } from "@/components/ui/tooltip"
+  import { Badge } from "@/components/ui/badge"
+
+  
 
 const socket = io('http://localhost:3001'); 
 interface Message {
@@ -27,7 +36,7 @@ export function Chat() {
         if (!socket.connected) {
             socket.connect();
         }
-        
+
         socket.on('connect', () => {
             console.log('Conectado ao WebSocket');
         });
@@ -73,111 +82,164 @@ export function Chat() {
         console.log(chatName);
     }
     
-
-return (
-    <div>
-        <div className='flex items-start'>
-            <ListCollapse size={32} className="block md:hidden text-white p-2 rounded" onClick={toggleTeams} />
-            <p className='text-white font-medium text-2xl'>Equipes</p>
-        </div>
-
-        <div className='flex flex-row'>
-            <div className={`flex flex-col ${isTeamsOpen ? 'block' : 'hidden'} md:block hidden-mobile`}>
-                <div className="flex flex-row mt-5 cursor-pointer shadow-shape bg-zinc-700 rounded-2xl w-auto min-w-96 items-center hover:bg-indigo-500" onClick={() => setNameChat('Equipe Desenvolvimento')}>
-                    <div className='flex flex-col items-center '>
-                    <Avatar className="w-20 h-20 flex items-center justify-center">
-                        <AvatarFallback className="bg-zinc-300 text-zinc-950 text-md p-3 rounded-3xl">
-                            {getInitials('Equipe Desenvolvimento')}
-                        </AvatarFallback>
-                    </Avatar>
-                    </div>
-                    <div className='flex flex-col'>
-                    <p className="text-white text-center flex items-center justify-center text-sm font-semibold">Equipe Desenvolvimento</p>
-                    </div>
-                </div>
-                <div className="flex flex-row mt-5 cursor-pointer shadow-shape bg-zinc-700 rounded-2xl w-auto min-w-96 items-center hover:bg-indigo-500" onClick={() => setNameChat('Equipe Suporte')}>
-                    <div className='flex flex-col items-center '>
-                    <Avatar className="w-20 h-20 flex items-center justify-center rounded-3xl">
-                        <AvatarFallback className="bg-zinc-300 text-zinc-950 text-md p-3 rounded-3xl">
-                            {getInitials('Equipe Suporte')}
-                        </AvatarFallback>
-                    </Avatar>
-                    </div>
-                    <div className='flex flex-col'>
-                        <p className="text-white text-center flex items-center justify-center text-sm font-semibold">Equipe Suporte Técnico</p>
-                    </div>
-                </div>
+    return (
+        <div>
+            <div className='flex items-center gap-4'>
+                <ListCollapse size={32} className="block md:hidden text-white p-2 rounded" onClick={toggleTeams} />
+                <p className='text-white font-medium text-2xl'>Cliente X</p>
+                <Badge className='bg-red-700'>Alta prioridade</Badge>
+                <TooltipProvider>
+                    <Tooltip>
+                        <TooltipTrigger>
+                            <Archive size={20} className="text-white items-center text-center cursor-pointer hover:text-indigo-400" />
+                        </TooltipTrigger>
+                        <TooltipContent className='border-zinc-700'>
+                            <p>Equipes arquivadas</p>
+                        </TooltipContent>
+                    </Tooltip>
+                </TooltipProvider>            
             </div>
 
-            {/* Área de chat */}
-            <div className={`flex flex-col w-full md:ml-10 ${!isTeamsOpen ? 'block' : 'hidden'}`}>
-                <div className='shadow-shape bg-zinc-800 mt-5 rounded-2xl flex-1 flex flex-col max-h-[700px] md:min-h-[600px] '>
-                    {/* Conteúdo do chat */}
-                    <div className="flex flex-row mb-5 cursor-pointer shadow-shape rounded-2xl w-auto min-w-96 items-center h-16">
-                    <div className='flex flex-col items-center'>
-                        <Avatar className="w-20 max-20 flex items-center justify-center rounded-3xl">
-                        <AvatarFallback className="bg-zinc-300 text-zinc-950 text-md p-3 rounded-3xl">
-                            {getInitials(chatName)}
-                        </AvatarFallback>
+            <div className='flex flex-row'>
+                <div className={`flex flex-col ${isTeamsOpen ? 'block' : 'hidden'} md:block hidden-mobile`}>
+                    <div className="flex flex-row mt-5 cursor-pointer shadow-shape bg-zinc-700 rounded-2xl w-auto min-w-96 items-center hover:bg-indigo-500" onClick={() => setNameChat('Equipe Desenvolvimento')}>
+                        <div className='flex flex-col items-center '>
+                        <Avatar className="w-20 h-20 flex items-center justify-center">
+                            <AvatarFallback className="bg-zinc-300 text-zinc-950 text-md p-3 rounded-3xl">
+                                {getInitials('Equipe Desenvolvimento')}
+                            </AvatarFallback>
                         </Avatar>
-                    </div>
-                    <div className='flex flex-col'>
-                        <p className='text-white font-bold text-xl'>{chatName}</p>
-                    </div>
-                    </div>
-
-                    <div className='flex flex-col flex-1 overflow-auto'>
-                    {messages.map((message, index) => (
-                        <div
-                        className={`message-container ${
-                            message.authorId === socket.id ? 'justify-end' : 'justify-start'
-                        } flex mb-2 items-center`}
-                        key={index}
-                        >
-                        {message.authorId !== socket.id && (
-                            <Avatar className="w-20 h-20 flex items-center justify-center mr-2 rounded-3xl">
-                            <AvatarFallback className="bg-zinc-300 text-zinc-950 text-sm p-3 rounded-3xl">
-                                {getInitials(message.author)}
-                            </AvatarFallback>
-                            </Avatar>
-                        )}
-                        <div className={`message ${message.authorId === socket.id ? 'bg-indigo-700 text-white' : 'bg-zinc-400 text-black'} p-2 rounded-lg max-w-xs `}>
-                            <div className="message-author font-bold max-w-xs">{message.author}</div>
-                            <div className="message-text max-w-lg">{message.text}</div>
                         </div>
-                        {message.authorId === socket.id && (
-                            <Avatar className="w-20 h-20 flex items-center justify-center ml-2 rounded-3xl">
-                            <AvatarFallback className="bg-indigo-300 text-zinc-950 text-sm p-3 rounded-3xl">
-                                {getInitials(message.author)}
-                            </AvatarFallback>
-                            </Avatar>
-                        )}
-                        <div ref={messagesEndRef} />
+                        <div className='flex flex-col'>
+                        <p className="text-white text-center flex items-center justify-center text-sm font-semibold">Equipe Desenvolvimento</p>
                         </div>
-                    ))}
-                    
                     </div>
+                    <div className="flex flex-row mt-5 cursor-pointer shadow-shape bg-zinc-700 rounded-2xl w-auto min-w-96 items-center hover:bg-indigo-500" onClick={() => setNameChat('Equipe Suporte')}>
+                        <div className='flex flex-col items-center '>
+                        <Avatar className="w-20 h-20 flex items-center justify-center rounded-3xl">
+                            <AvatarFallback className="bg-zinc-300 text-zinc-950 text-md p-3 rounded-3xl">
+                                {getInitials('Equipe Suporte')}
+                            </AvatarFallback>
+                        </Avatar>
+                        </div>
+                        <div className='flex flex-col'>
+                            <p className="text-white text-center flex items-center justify-center text-sm font-semibold">Equipe Suporte Técnico</p>
+                        </div>
+                    </div>
+                    <div className="flex flex-row mt-5 cursor-pointer shadow-shape border border-zinc-600 rounded-2xl w-auto min-w-96 items-center hover:bg-zinc-800" onClick={() => setNameChat('Equipe Suporte')}>
+                        <div className='flex flex-col items-center '>
+                            <CirclePlus className="cursor-pointer w-12 h-20 ml-3 mr-4 flex items-center justify-center rounded-3xl" />
+                        </div>
+                        <div className='flex flex-col'>
+                            <p className="text-white text-center flex items-center justify-center text-sm font-semibold">Novo grupo</p>
+                        </div>
+                    </div>
+                </div>
 
-                    <div className="relative flex items-center bg-zinc-900 border-zinc-800 rounded-xl w-full mt-5">
-                        <Input 
-                            type='text'
-                            placeholder='Digite uma mensagem...'
-                            value={message}
-                            onChange={(e) => setMessage(e.target.value)}
-                            onKeyDown={(e) => {
-                            if (e.key === 'Enter') {
-                                sendMessage();
-                                e.preventDefault(); 
-                            }
-                            }}
-                            className="pl-5 pr-4 py-2 text-md rounded-2xl h-12 w-full max-w-4xl border bg-transparent border-none shadow-shape" 
-                        />
-                        <SendHorizonal onClick={sendMessage} size={24} className="absolute right-3 text-indigo-400 cursor-pointer" />
+                <div className={`flex flex-col w-full md:ml-10 ${!isTeamsOpen ? 'block' : 'hidden'}`}>
+                    <div className='shadow-shape bg-zinc-800 mt-5 rounded-2xl flex-1 flex flex-col max-h-[700px] md:min-h-[600px] '>
+                        <div className="flex flex-row mb-5 cursor-pointer shadow-shape rounded-2xl w-auto min-w-96 items-center h-16">
+                            <div className='flex flex-col items-center'>
+                                <Avatar className="w-20 max-20 flex items-center justify-center rounded-3xl">
+                                    <AvatarFallback className="bg-zinc-300 text-zinc-950 text-md p-3 rounded-3xl">
+                                        {getInitials(chatName)}
+                                    </AvatarFallback>
+                                </Avatar>
+                            </div>
+                            <div className='flex flex-row flex-grow items-center justify-between'>
+                                <div className='flex h-10 items-center'>
+                                    <p className='text-white font-bold text-xl'>{chatName}</p>
+                                    <Badge className='bg-red-700 ml-5'>Alta prioridade</Badge>
+                                </div>
+
+                                <div className='flex h-10 items-center space-x-5 mr-5'>
+                                    <TooltipProvider>
+                                        <Tooltip>
+                                            <TooltipTrigger>
+                                                <Link2 size={24} className="text-white cursor-pointer hover:text-indigo-400" />
+                                            </TooltipTrigger>
+                                            <TooltipContent className='border-zinc-700 bg-zinc-800'>
+                                                <p>Links importantes</p>
+                                            </TooltipContent>
+                                        </Tooltip>
+                                    </TooltipProvider>  
+                                    <TooltipProvider>
+                                        <Tooltip>
+                                            <TooltipTrigger>
+                                                <UserPlus2 size={24} className="text-white cursor-pointer hover:text-indigo-400" />
+                                            </TooltipTrigger>
+                                            <TooltipContent className='border-zinc-700 bg-zinc-800'>
+                                                <p>Participantes</p>
+                                            </TooltipContent>
+                                        </Tooltip>
+                                    </TooltipProvider>    
+                                    <TooltipProvider>
+                                        <Tooltip>
+                                            <TooltipTrigger>
+                                                <Settings size={24} className="text-white cursor-pointer hover:text-indigo-400" />
+                                            </TooltipTrigger>
+                                            <TooltipContent className='border-zinc-700 bg-zinc-800'>
+                                                <p>Configurações</p>
+                                            </TooltipContent>
+                                        </Tooltip>
+                                    </TooltipProvider>    
+                                    
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div className='flex flex-col flex-1 overflow-auto'>
+                        {messages.map((message, index) => (
+                            <div
+                            className={`message-container ${
+                                message.authorId === socket.id ? 'justify-end' : 'justify-start'
+                            } flex mb-2 items-center`}
+                            key={index}
+                            >
+                            {message.authorId !== socket.id && (
+                                <Avatar className="w-20 h-20 flex items-center justify-center mr-2 rounded-3xl">
+                                <AvatarFallback className="bg-zinc-300 text-zinc-950 text-sm p-3 rounded-3xl">
+                                    {getInitials(message.author)}
+                                </AvatarFallback>
+                                </Avatar>
+                            )}
+                            <div className={`message ${message.authorId === socket.id ? 'bg-indigo-700 text-white' : 'bg-zinc-400 text-black'} p-2 rounded-lg max-w-xs `}>
+                                <div className="message-author font-bold max-w-xs">{message.author}</div>
+                                <div className="message-text max-w-lg">{message.text}</div>
+                            </div>
+                            {message.authorId === socket.id && (
+                                <Avatar className="w-20 h-20 flex items-center justify-center ml-2 rounded-3xl">
+                                <AvatarFallback className="bg-indigo-300 text-zinc-950 text-sm p-3 rounded-3xl">
+                                    {getInitials(message.author)}
+                                </AvatarFallback>
+                                </Avatar>
+                            )}
+                            <div ref={messagesEndRef} />
+                            </div>
+                        ))}
+                        
+                        </div>
+
+                        <div className="relative flex items-center bg-zinc-900 border-zinc-800 rounded-xl w-full mt-5">
+                            <Input 
+                                type='text'
+                                placeholder='Digite uma mensagem...'
+                                value={message}
+                                onChange={(e) => setMessage(e.target.value)}
+                                onKeyDown={(e) => {
+                                if (e.key === 'Enter') {
+                                    sendMessage();
+                                    e.preventDefault(); 
+                                }
+                                }}
+                                className="pl-5 pr-4 py-2 text-md rounded-2xl h-12 w-full max-w-4xl border bg-transparent border-none shadow-shape" 
+                            />
+                            <SendHorizonal onClick={sendMessage} size={24} className="absolute right-3 text-indigo-400 cursor-pointer" />
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-
-  );
+    );
 }
