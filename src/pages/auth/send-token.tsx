05cade6cac07a67d5ec804a22809bf12698af4ed
Eduarda Brisch Dalmás <du.dalmas@hotmail.com>
2024-11-10 10:43:27 -0300

    loading nas telas de login e send token

--- src/pages/auth/send-token.tsx
+++ src/pages/auth/send-token.tsx
@@ -11,6 +11,7 @@ import { useNavigate, useParams } from 'react-router-dom';
 import { verifyToken } from '@/http/verify-token';
 import { useState } from 'react';
 import { useAuth } from '@/context/AuthContext';
+import { PropagateLoader } from 'react-spinners';
 
 const FormSchema = z.object({
     pin: z.string().min(6, {
@@ -25,6 +26,7 @@ export function SendToken() { // Página de envio de token
     const [Sucess, setSuccess] = useState('');
     const { setEmail, setName } = useAuth();
     const { login } = useAuth();
+    const [loading, setLoading] = useState(false);
 
 
     const form = useForm<z.infer<typeof FormSchema>>({
@@ -36,16 +38,13 @@ export function SendToken() { // Página de envio de token
 
     async function onSubmit(data: z.infer<typeof FormSchema>) { // Função para lidar com o envio do formulário
         toast(`You submitted the following values: ${JSON.stringify(data, null, 2)}`)
+        setLoading(true);
 
         if (!data) {
             return toast.error('Insira o token enviado para seu email para acessar sua conta!')
         }
         try {
-
-
             const response = await verifyToken({ pin: data.pin, email: email });
-
-
 
             if (response) {
                 if (response.success) {
@@ -61,12 +60,14 @@ export function SendToken() { // Página de envio de token
                     login(token);
                     setError(""); // Limpa qualquer mensagem de erro anterior
                     if (response.message.user.profiles === null) {
+                        setLoading(false);
                         navigate('/profiles'); // Navega para a página inicial
-
                     } else {
+                        setLoading(false);
                         navigate('/home/' + response.message.user.profiles);
                     }
                 } else {
+                    setLoading(false);
                     setError(response.message); // Mensagem de erro
                     setSuccess(""); // Limpa qualquer mensagem de sucesso anterior
                     setTimeout(() => {
@@ -74,12 +75,14 @@ export function SendToken() { // Página de envio de token
                       }, 3000); // 3 segundos
                 }
             } else {
+                setLoading(false);
                 setError('Erro ao verificar o token. Tente novamente.'); // Caso a resposta seja undefined
                 setTimeout(() => {
                     setError('');
                   }, 3000); // 3 segundos
             }
         } catch (error) {
+            setLoading(false);
             console.error(error);
             setError('Erro ao verificar o token. Tente novamente.');
             setSuccess(""); // Limpa qualquer mensagem de sucesso anterior
@@ -92,86 +95,91 @@ export function SendToken() { // Página de envio de token
 
     return (
         
-        
-
-
-        <div className="flex items-center justify-center">
-            {/* Alertas de Sucesso e Erro */}
-            <div className="absolute top-12 left-1/2 transform -translate-x-1/2 mb-4 z-10">
-                {Sucess && (
-                    <div className="bg-green-500 text-white text-sm font-semibold rounded-md shadow-lg p-3 flex items-center max-w-xs mx-auto">
-                        <svg className="w-4 h-4 mr-2" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M5 13l4 4L19 7" />
-                        </svg>
-                        {Sucess}
-                    </div>
-                )}
-
-                {Error && (
-                    <div className="bg-red-500 text-white text-sm font-semibold rounded-md shadow-lg p-3 flex items-center max-w-xs mx-auto">
-                        <svg className="w-4 h-4 mr-2" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-                        </svg>
-                        {Error}
-                    </div>
-                )}
+        <div>
+            {loading ? (
+            <div className="fixed inset-0 flex items-center justify-center z-50 bg-zinc-900">
+            <PropagateLoader color="#6366F1" loading={loading} size={15} />
             </div>
+            ) : (
+            <div className="flex items-center justify-center">
+                {/* Alertas de Sucesso e Erro */}
+                <div className="absolute top-12 left-1/2 transform -translate-x-1/2 mb-4 z-10">
+                    {Sucess && (
+                        <div className="bg-green-500 text-white text-sm font-semibold rounded-md shadow-lg p-3 flex items-center max-w-xs mx-auto">
+                            <svg className="w-4 h-4 mr-2" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M5 13l4 4L19 7" />
+                            </svg>
+                            {Sucess}
+                        </div>
+                    )}
 
-            <div className="max-w-lg w-full px-6 text-center space-y-10 bg-zinc-800 h-96 rounded-xl">
+                    {Error && (
+                        <div className="bg-red-500 text-white text-sm font-semibold rounded-md shadow-lg p-3 flex items-center max-w-xs mx-auto">
+                            <svg className="w-4 h-4 mr-2" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                            </svg>
+                            {Error}
+                        </div>
+                    )}
+                </div>
 
-                <div className="flex flex-col items-center gap-3 pt-3">
+                <div className="max-w-lg w-full px-6 text-center space-y-10 bg-zinc-800 h-96 rounded-xl">
 
-                    <img
-                        src={logo}
-                        alt="Logo ColabFlow"
-                        className="h-32 w-auto mx-auto m-3"
-                    />
-                    <Form {...form}>
-                        <form
-                            onSubmit={form.handleSubmit(onSubmit)}
-                            className=' border-zinc-800 flex flex-col items-center justify-center'
-                        >
-                            <FormField
-                                control={form.control}
-                                name="pin"
-                                render={({ field }) => (
-                                    <FormItem className='flex flex-col items-center justify-center text-center'>
-                                        <FormLabel className='font-medium text-md mb-4'>Insira o código que foi enviado para seu e-mail</FormLabel>
-                                        <FormControl>
-                                            <InputOTP maxLength={6} {...field}>
-                                                <div className="flex gap-2">
-                                                    <InputOTPGroup>
-                                                        <InputOTPSlot index={0} />
-                                                        <InputOTPSlot index={1} />
-                                                        <InputOTPSlot index={2} />
-                                                    </InputOTPGroup>
-                                                    <InputOTPSeparator />
-                                                    <InputOTPGroup>
-                                                        <InputOTPSlot index={3} />
-                                                        <InputOTPSlot index={4} />
-                                                        <InputOTPSlot index={5} />
-                                                    </InputOTPGroup>
-                                                </div>
-                                            </InputOTP>
-                                        </FormControl>
-                                        <FormMessage />
-                                    </FormItem>
-                                )}
-                            />
-                            <Button
-                                type="submit"
-                                className="bg-indigo-700 border-none text-base text-white font-bold rounded-2xl h-12 w-64 mt-5 hover:bg-indigo-500"
+                    <div className="flex flex-col items-center gap-3 pt-3">
+
+                        <img
+                            src={logo}
+                            alt="Logo ColabFlow"
+                            className="h-32 w-auto mx-auto m-3"
+                        />
+                        <Form {...form}>
+                            <form
+                                onSubmit={form.handleSubmit(onSubmit)}
+                                className=' border-zinc-800 flex flex-col items-center justify-center'
                             >
-                                Acessar
-                            </Button>
-                            
+                                <FormField
+                                    control={form.control}
+                                    name="pin"
+                                    render={({ field }) => (
+                                        <FormItem className='flex flex-col items-center justify-center text-center'>
+                                            <FormLabel className='font-medium text-md mb-4'>Insira o código que foi enviado para seu e-mail</FormLabel>
+                                            <FormControl>
+                                                <InputOTP maxLength={6} {...field}>
+                                                    <div className="flex gap-2">
+                                                        <InputOTPGroup>
+                                                            <InputOTPSlot index={0} />
+                                                            <InputOTPSlot index={1} />
+                                                            <InputOTPSlot index={2} />
+                                                        </InputOTPGroup>
+                                                        <InputOTPSeparator />
+                                                        <InputOTPGroup>
+                                                            <InputOTPSlot index={3} />
+                                                            <InputOTPSlot index={4} />
+                                                            <InputOTPSlot index={5} />
+                                                        </InputOTPGroup>
+                                                    </div>
+                                                </InputOTP>
+                                            </FormControl>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+                                <Button
+                                    type="submit"
+                                    className="bg-indigo-700 border-none text-base text-white font-bold rounded-2xl h-12 w-64 mt-5 hover:bg-indigo-500"
+                                >
+                                    Acessar
+                                </Button>
+                                
 
-                        </form>
-                    </Form>
-                    
+                            </form>
+                        </Form>
+                        
 
+                    </div>
                 </div>
             </div>
-        </div>
+         )}
+      </div>
     )
 }
--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -7,6 +7,7 @@ import { User } from "lucide-react";
 import { FormEvent, useEffect, useState } from 'react';
 import { toast } from 'sonner';
 import { motion } from 'framer-motion';
+import { PropagateLoader } from 'react-spinners';
 // @ts-ignore
 import { useAuth } from '../../context/AuthContext';
 
@@ -15,6 +16,7 @@ import { getTokenEmail } from '@/http/get-token-email';
 
 export function SignIn() {
     const [isStruck, setIsStruck] = useState(false);
+    const [loading, setLoading] = useState(false);
 
     const navigate = useNavigate();
     // const { login } = useAuth();
@@ -40,6 +42,7 @@ export function SignIn() {
       
     async function handleSubmit(event: FormEvent<HTMLFormElement>) {
         event.preventDefault();
+        setLoading(true);
 
         try {
             const data = new FormData(event.currentTarget);
@@ -52,8 +55,10 @@ export function SignIn() {
             const response = await getTokenEmail({ email });
 
             if (response) {
+                setLoading(false);
                 navigate(`/send-token/${email}`);
             } else {
+                setLoading(false);
                 toast.error('E-mail não encontrado. Tente novamente!');
             }
 
@@ -67,100 +72,108 @@ export function SignIn() {
     }
 
     return (
-        <div className="relative h-screen grid grid-cols-1 md:grid-cols-2">
+        <div>
+        {loading ? (
+           <div className="fixed inset-0 flex items-center justify-center z-50 bg-zinc-900">
+           <PropagateLoader color="#6366F1" loading={loading} size={15} />
+         </div>
+        ) : (
+          <div className="relative h-screen grid grid-cols-1 md:grid-cols-2">
             {/* Coluna da Esquerda */}
             <div className="relative flex flex-col items-center justify-center h-full p-8">
-                <div className="relative flex flex-col items-center text-center z-10 px-4 md:px-8">
-                    <motion.h1
-                        className="text-2xl md:text-3xl mb-4 md:mb-6 text-indigo-400 max-w-md relative"
-                        initial={{ opacity: 0 }}
-                        animate={{ opacity: 1 }}
-                        transition={{ duration: 1 }}
-                        >
-                        <motion.span
-                            className="inline-block"
+              <div className="relative flex flex-col items-center text-center z-10 px-4 md:px-8">
+                <motion.h1
+                  className="text-2xl md:text-3xl mb-4 md:mb-6 text-indigo-400 max-w-md relative"
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ duration: 1 }}
+                >
+                  <motion.span
+                    className="inline-block"
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 1, delay: 0.3 }}
+                  >
+                    {words.map((word, wordIndex) => (
+                      <span key={wordIndex} className="inline-block mr-2">
+                        {word.split('').map((letter, letterIndex) => (
+                          <motion.span
+                            key={letterIndex}
+                            className={`relative inline-block ${isStruck ? 'animate-strike' : ''}`} // Aplica o efeito de risco se necessário
                             initial={{ opacity: 0 }}
                             animate={{ opacity: 1 }}
-                            transition={{ duration: 1, delay: 0.3 }}
-                        >
-                            {words.map((word, wordIndex) => (
-                            <span key={wordIndex} className="inline-block mr-2">
-                                {word.split('').map((letter, letterIndex) => (
-                                <motion.span
-                                    key={letterIndex}
-                                    className={`relative inline-block ${isStruck ? 'animate-strike' : ''}`} // Aplica o efeito de risco se for necessário
-                                    initial={{ opacity: 0 }}
-                                    animate={{ opacity: 1 }}
-                                    transition={{
-                                    opacity: { delay: (wordIndex * 0.3) + (letterIndex * 0.1) }, // Controla o delay para cada letra dentro da palavra
-                                    duration: 0.5, // Duração de cada letra aparecer
-                                    }}
-                                >
-                                    {letter}
-                                </motion.span>
-                                ))}
-                            </span>
-                            ))}
-                        </motion.span>
-                    </motion.h1>
-                    <img
-                        src={logo}
-                        alt="Logo"
-                        className="w-40 h-40 md:w-72 md:h-52 mb-4 md:mb-6 object-contain"
-                    />
-                    <h2 className="text-sm md:text-lg mb-4 max-w-md leading-6 md:leading-7 font-extralight text-white">
-                        Chat colaborativo que organiza as atividades dos seus times, ajudando sua equipe a priorizar e manter o foco no que realmente importa.
-                    </h2>
-                    <Button
-                        onClick={CreateAccount}
-                        className="bg-indigo-700 border-none text-sm md:text-base text-white font-bold rounded-2xl h-10 md:h-12 w-56 md:w-64 mt-5 hover:bg-indigo-500 shadow-shape"
-                    >
-                        Criar conta
-                    </Button>
-                </div>
+                            transition={{
+                              opacity: { delay: (wordIndex * 0.3) + (letterIndex * 0.1) },
+                              duration: 0.5,
+                            }}
+                          >
+                            {letter}
+                          </motion.span>
+                        ))}
+                      </span>
+                    ))}
+                  </motion.span>
+                </motion.h1>
+                <img
+                  src={logo}
+                  alt="Logo"
+                  className="w-40 h-40 md:w-72 md:h-52 mb-4 md:mb-6 object-contain"
+                />
+                <h2 className="text-sm md:text-lg mb-4 max-w-md leading-6 md:leading-7 font-extralight text-white">
+                  Chat colaborativo que organiza as atividades dos seus times, ajudando sua equipe a priorizar e manter o foco no que realmente importa.
+                </h2>
+                <Button
+                  onClick={CreateAccount}
+                  className="bg-indigo-700 border-none text-sm md:text-base text-white font-bold rounded-2xl h-10 md:h-12 w-56 md:w-64 mt-5 hover:bg-indigo-500 shadow-shape"
+                >
+                  Criar conta
+                </Button>
+              </div>
             </div>
-
+  
             {/* Coluna da Direita */}
             <div className="relative flex items-center justify-center h-full">
-                <img
-                    src={bg}
-                    alt="Background"
-                    className="absolute inset-0 h-full w-full object-cover z-[-1]"
-                />
-                <div className="relative max-w-lg w-full px-6 text-center space-y-10 bg-zinc-800 min-h-96 rounded-xl shadow-shape z-10">
-                    <div className="flex flex-col items-center gap-3 pt-3">
-                        <img
-                            src={logo}
-                            alt="Logo do ColabFlow"
-                            className="h-32 w-auto mx-auto m-3"
-                        />
-
-                        <form 
-                            onSubmit={handleSubmit}
-                            className='flex flex-col border border-zinc-800 items-center justify-center'
-                        > 
-                            <h1 className="text-xl font-medium text-white mb-5">Login</h1>
-                            <div className="relative flex items-center bg-zinc-950 border-zinc-800 rounded-xl max-w-sm">
-                                <User size={24} className="absolute left-3 text-gray-400" />
-                                <Input 
-                                    name='email'
-                                    type="email" 
-                                    placeholder="Email"  
-                                    className="pl-12 pr-4 py-2 text-md rounded-2xl h-12 md:w-80 border bg-transparent border-none shadow-shape" 
-                                />
-                            </div>
-                            {error && <p className="text-red-500 mb-4">{error}</p>}
-
-                            <Button
-                                type="submit"
-                                className="bg-indigo-700 border-none text-base text-white font-bold rounded-2xl h-12 w-64 mt-5 hover:bg-indigo-500 shadow-shape"
-                            >
-                                Enviar código
-                            </Button>
-                        </form>
+              <img
+                src={bg}
+                alt="Background"
+                className="absolute inset-0 h-full w-full object-cover z-[-1]"
+              />
+              <div className="relative max-w-lg w-full px-6 text-center space-y-10 bg-zinc-800 min-h-96 rounded-xl shadow-shape z-10">
+                <div className="flex flex-col items-center gap-3 pt-3">
+                  <img
+                    src={logo}
+                    alt="Logo do ColabFlow"
+                    className="h-32 w-auto mx-auto m-3"
+                  />
+  
+                  <form
+                    onSubmit={handleSubmit}
+                    className="flex flex-col border border-zinc-800 items-center justify-center"
+                  >
+                    <h1 className="text-xl font-medium text-white mb-5">Login</h1>
+                    <div className="relative flex items-center bg-zinc-950 border-zinc-800 rounded-xl max-w-sm">
+                      <User size={24} className="absolute left-3 text-gray-400" />
+                      <Input
+                        name="email"
+                        type="email"
+                        placeholder="Email"
+                        className="pl-12 pr-4 py-2 text-md rounded-2xl h-12 md:w-80 border bg-transparent border-none shadow-shape"
+                      />
                     </div>
+                    {error && <p className="text-red-500 mb-4">{error}</p>}
+  
+                    <Button
+                      type="submit"
+                      className="bg-indigo-700 border-none text-base text-white font-bold rounded-2xl h-12 w-64 mt-5 hover:bg-indigo-500 shadow-shape"
+                    >
+                      Enviar código
+                    </Button>
+                  </form>
                 </div>
+              </div>
             </div>
-        </div>
+          </div>
+        )}
+      </div>
     );
-}
+  };
